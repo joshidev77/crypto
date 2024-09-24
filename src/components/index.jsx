b6d8072030a0d9da5e05355c532b4d2a0779f6c0
Joshi Dev Jitenkumar <139579654+joshidev77@users.noreply.github.com>
2024-09-24 10:09:47 +0530

    Update index.jsx

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,7 +3,7 @@ import RegisterContainer from "./register/RegisterContainer";
 import LoginContainer from "./login/LoginContainer";
 import CampaignListContainer from "./campaignList/CampaignListContainer";
 import CreateCampaignContainer from "./createCampaign/CreateCampaignContainer";
-import UserDashBoardContainer from "./userdashboard/UserDashBoardContainer";
+import UserDashBoardContainer from "./userDashboard/UserDashBoardContainer";
 import CampaignDetailsContainer from "./campaigndetails/CampaignDetailsContainer";
 import ContactUsContainer from "./contactus/ContactUsContainer";
 import WithDrawContainer from "./withdraw/WithDrawContainer";
